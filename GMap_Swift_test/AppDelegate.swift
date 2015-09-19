dd048f6a28bcbc13c55039b0d5331b5ad098223a
Xi Yang <clcblueljw@gmail>
2015-09-19 19:21:55 -0400

    Finshed initial Google Map

--- GMap_Swift_test/AppDelegate.swift
+++ GMap_Swift_test/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyC6EOlM5-zhRYDHNYi0fc2xU5jtsTzadcs");
+        
         return true
     }
 
